480b2279f8eef1802029bb0c39683f221d067fa0
Paul Hodges <paul.hodges@student.education.wa.edu.au>
2023-09-11 17:48:16 +0800

    Created window

--- src/main.py
+++ src/main.py
@@ -2,5 +2,8 @@ import sys
 import os
 import pygame
 
-import tailwind.window as window
+import tailwind.window
 import tailwind.util as util
+
+window = tailwind.window.Window(None, "Maths CAT Investigation", util.WindowProperties.empty(), True, debug=True)
+
